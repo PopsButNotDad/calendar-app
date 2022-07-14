0de20785885f213118a36fc0c40d46b60100eb23
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-07-14 15:45:21 -0700

    Architecture work

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello There!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+// Calendar app structure
+
+// single page app
+
+// <-- holding div --> 
+
+//   big arrow | lil arrow | Month | Year | lil arrow | big arrow 
+
+//   <-- day label holding div -->
+
+//     sunday | monday | tuesday | wednesday | thursday | friday | saturday
+
+//   <-- close day label holding div -->
+
+//   <-- calendar boxes holding div -->
+
+//   SHOULD PROBABLY ONLY RENDER THE ONES WITHIN THAT MONTH AND GREY OUT THE OTHERS
+
+//     | |   | |   | |   | |   | |   | |   | | 
+//     | |   | |   | |   | |   | |   | |   | | 
+//     | |   | |   | |   | |   | |   | |   | | 
+//     | |   | |   | |   | |   | |   | |   | | 
+//     | |   | |   | |   | |   | |   | |   | | 
+//     | |   | |   | |   | |   | |   | |   | | 
+
+
+//   <-- close calendar boxes holding div -->
+
+
+
+
+// <-- close holding div -->
