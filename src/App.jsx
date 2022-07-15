d83fc6f61e45e5ae190220798e56c2346ffb1b72
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-07-15 12:55:39 -0700

    working on buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+
+
 function App() {
+  const cd = new Date();
+  const date = `${cd.getMonth()+1}/${cd.getDate()}/${cd.getFullYear()}`;
+
+  console.log(cd.getMonth());
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello There!</p>
+        <h1>National Holiday Calendar for USA</h1>
+        <button >Left Big Arrow</button>
+        <button >Left Small Arrow</button>
+          <select name="months" className="monthPicker">
+            <option value={0}>January</option>
+            <option value={1}>February</option>
+            <option value={2}>March</option>
+            <option value={3}>April</option>
+            <option value={4}>May</option>
+            <option value={5}>June</option>
+            <option value={6}>July</option>
+            <option value={7}>August</option>
+            <option value={8}>September</option>
+            <option value={9}>October</option>
+            <option value={10}>November</option>
+            <option value={11}>December</option>
+          </select>
+        <button >Right Small Arrow</button>
+        <button >Right Big Arrow</button>
+        <p> Current Date: {date} </p>
       </header>
     </div>
   );
